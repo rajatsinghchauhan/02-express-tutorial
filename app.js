4944a7e0bc74f105473c31bf47104f5ba76b43d0
rajatsinghchauhan <singhrajatmyworld@gmail.com>
2022-03-08 17:45:32 +0530

    location changed

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-const http = require("http");
+const express = require("express");
 
-const { readFileSync } = require("fs");
+const { products } = require("./data");
 
-const homepage = readFileSync("./navbar-app/index.html");
-const homestyle = readFileSync("./navbar-app/styles.css");
-const homelogo = readFileSync("./navbar-app/logo.svg");
-const homelogic = readFileSync("./navbar-app/browser-app.js");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  console.log(url);
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homepage);
-    res.end();
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homestyle);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homelogic);
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1> About page </h1 > ");
-    res.end();
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homelogo);
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Page not found </h1 > ");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.json(products);
 });
 
-server.listen(5000);
+app.listen(5000, () => {
+  console.log("hey i am listening it ");
+});
