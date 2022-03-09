f8138c0883de1125900a47678211cdbb3a99d752
rajatsinghchauhan <singhrajatmyworld@gmail.com>
2022-03-09 17:55:38 +0530

    added express.json

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const { products } = require("./data");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send("<h1>Home page </h1> <a href='/api/products'> products</a>");
 });
+app.get("/api/products", (req, res) => {
+  const newproducts = products.map((product) => {
+    const { id, name, image } = product;
+    return {
+      id,
+      name,
+      image,
+    };
+  });
 
+  res.json(newproducts);
+});
 app.listen(5000, () => {
   console.log("hey i am listening it ");
 });
