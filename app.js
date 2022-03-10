57635b4f0913e461b6540694b914b9230092c72c
rajatsinghchauhan <singhrajatmyworld@gmail.com>
2022-03-10 23:42:52 +0530

    minor fix

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 const express = require("express");
 
-const { products } = require("./data");
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Home page </h1> <a href='/api/products'> products</a>");
-});
-app.get("/api/products", (req, res) => {
-  const newproducts = products.map((product) => {
-    const { id, name, image } = product;
-    return {
-      id,
-      name,
-      image,
-    };
-  });
-
-  res.json(newproducts);
-});
 app.listen(5000, () => {
-  console.log("hey i am listening it ");
+  console.log("heloo i am listening ");
 });
